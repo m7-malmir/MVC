ac80f4ae62581abdc54610d6619a221c8a197bc7
m7-malmir <m.malmir666@gmail.com>
2024-07-02 16:22:35 +0330

    necessary folder was add

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+//namespace app\core;
+require_once __DIR__.'/../vendor/autoload.php';
+use app\core\Application;
+
+
+$app=new Application();
+
+$app->router->get('/',function(){
+    return 'hello world';
+});
+$app->router->get('/contact',function(){
+    return 'Contact Page';
+});
+
+$app->run();
