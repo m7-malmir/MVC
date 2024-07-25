232a43a30a8ec56377e5a0c3ce58110ce43dadf4
m7-malmir <m.malmir666@gmail.com>
2024-07-25 14:51:50 +0330

    three file add

--- core/Router.php
+++ core/Router.php
@@ -1,8 +1,6 @@
 <?php
 namespace app\core;
-
 use app\core\Requset;
-
 class Router{
     public Requset $request;
     public Response $response;
@@ -11,42 +9,42 @@ class Router{
       $this->request=$request;
       $this->response=$response;
     }
-     public function get($path,$callback){
+    public function get($path,$callback){
         $this->routes['get'][$path]=$callback;
      }
-     public function post($path,$callback){
-      $this->routes['post'][$path]=$callback;
-   }
+    public function post($path,$callback){
+        $this->routes['post'][$path]=$callback;
+    }
     public function resolve(){
-       $path= $this->request->getPath();
-       $method=$this->request->getMethod();
-       $callback=$this->routes[$method][$path] ?? false;
-      if($callback===false){
-        $this->response->setStatusCode(404);
-        return $this->renderView("_404");
-      }
-      if(is_string($callback)){
-        return $this->renderView($callback);
-      }
-    return call_user_func($callback);
-     }
-     public function renderView($view) {
+        $path= $this->request->getPath();
+        $method=$this->request->getMethod();
+        $callback=$this->routes[$method][$path] ?? false;
+        if($callback===false){
+          $this->response->setStatusCode(404);
+          return $this->renderView("_404");
+        }
+        if(is_string($callback)){
+          return $this->renderView($callback);
+        }
+      return call_user_func($callback);
+    }
+    public function renderView($view) {
+        $layoutContent=$this->layoutContent();
+        $viewContent=$this->renderOnlyView($view);
+        return str_replace('{{content}}',$viewContent,$layoutContent);
+    }
+    public function renderContent($viewContent) {
       $layoutContent=$this->layoutContent();
-      $viewContent=$this->renderOnlyView($view);
       return str_replace('{{content}}',$viewContent,$layoutContent);
-     }
-     public function renderContent($viewContent) {
-      $layoutContent=$this->layoutContent();
-      return str_replace('{{content}}',$viewContent,$layoutContent);
-     }
-     protected function layoutContent(){
+    }
+    protected function layoutContent(){
       ob_start();
       include_once Application::$ROOT_DIR."/views/layouts/main.php";
       return ob_get_clean();
-     }
-     protected function renderOnlyView($view){
+    }
+    protected function renderOnlyView($view){
       ob_start();
       include_once Application::$ROOT_DIR."/views/$view.php";
       return ob_get_clean();
-     }
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
 <?php
-//namespace app\core;
+
 require_once __DIR__.'/../vendor/autoload.php';
 use app\controllers\SiteController;
 use app\core\Application;
 
-
 $app=new Application(dirname(__DIR__));
-
 $app->router->get('/','home');
 $app->router->get('/contact',[SiteController::class,'contact']);
 $app->router->post('/contact',[SiteController::class,'handleContact']);
-
 $app->run();
